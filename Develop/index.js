ec5b8871e53a017f3772261d09ccf89c58098f23
kalho <luisenriquez11@hotmail.com>
2022-02-27 12:24:39 -0500

    questions and prompt

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,75 @@
 // TODO: Include packages needed for this application
-
-// TODO: Create an array of questions for user input
-const questions = [];
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown.js");
+// TODO: Create an array of messages for user input
+const questions = [
+    {
+        name: "title",
+        type: "input",
+        message: "What is the title of your project?"
+    },
+    {
+        name: "description",
+        type: "input",
+        message: "Enter a brief description for your project:"
+    },
+    {
+        name: "install",
+        type: "input",
+        message: "Explain how to install your program:"
+    },
+    {
+        name: "usage",
+        type: "input",
+        message: "Explain how to use your program:"
+    },
+    {
+        name: "contribute",
+        type: "input",
+        message: "Explain how other people can contribute in your project:"
+    },
+    {
+        name: "test",
+        type: "input",
+        message: "Explain how other people can test your program:"
+    },
+    {
+        name: "license",
+        type: "list",
+        message: "Which of the following license would you like to use?",
+        choices: [
+            "GNU AGPLv3",
+            "GNU GPLv3",
+            "GNU LGPLv3",
+            "Mozilla Public License 2.0",
+            "Apache License 2.0",
+            "MIT License",
+            "Boost Software License 1.0",
+            "The Unlicense"
+        ]
+    },
+    {
+        name: "username",
+        type: "input",
+        message: "Enter your github username:"
+    },
+    {
+        name: "email",
+        type: "input",
+        message: "Enter your email address:"
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions);
+}
 
 // Function call to initialize app
 init();
